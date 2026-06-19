8bf9fa28eecce979179e72e3fc8c7a0bf77945f6
Sami <sowusuaboagye@gmail.com>
2026-06-19 17:47:14 +0000

    Changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 // App
 const app = require("./app")
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
 	console.log(`Relay API running on port ${PORT}`)
